c89799670e825bb21af7de271ce6e670f72a79a9
Vic P <vic4key@gmail.com>
2020-01-07 01:45:25 +0700

    Initial

--- fo.py
+++ fo.py
@@ -105,7 +105,7 @@ def main():
 			print(f"[ERROR] Unsupported the action `{action}` for the command `{command}`")
 			return -1
 
-		# Check the action directory
+		# check the action directory
 
 		is_required_dir = action in ["copy", "move"]
 
